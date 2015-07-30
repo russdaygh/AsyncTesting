56dd9d60e88f3d8900a42ab3eba514dc3ddc2cad
Russell Day <russell.day.coder@gmail.com>
2015-07-30 18:03:51 +0100

    Initial commit of async testing solution.

---
+++ AsyncTesting.Tests/MyControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AsyncTesting.Tests
+{
+    [TestClass]
+    public class MyControllerTests
+    {
+        [TestMethod]
+        public void BeginPasswordReset_SendsEmail()
+        {
+            const string emailAddress = "email@domain.com";
+
+            var sendCalled = new ManualResetEvent(false);
+
+            var mockEmailService = new Mock<IEmailService>();
+            mockEmailService.Setup(m => m.Send(emailAddress)).Callback(() =>
+            {
+                sendCalled.Set();
+            });
+            var controller = new MyController(mockEmailService.Object);            
+
+            controller.BeginPasswordReset(emailAddress);
+
+            Assert.IsTrue(sendCalled.WaitOne(TimeSpan.FromSeconds(3)), "Send was never called");
+            mockEmailService.Verify(es => es.Send(emailAddress));
+        }
+    }
+}
---
+++ AsyncTesting/AsyncTesting.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading;
+
+namespace AsyncTesting
+{
+    public class MyController : Controller
+    {
+        public IEmailService EmailService { get; set; }
+
+        public MyController(IEmailService emailService)
+        {
+            EmailService = emailService;
+        }
+
+        public ViewResult BeginPasswordReset(string emailAddress)
+        {
+            BeginPasswordResetAsync(emailAddress);
+
+            return View();
+        }
+
+        private Task BeginPasswordResetAsync(string emailAddress)
+        {
+            return Task.Run(delegate
+            {
+                Thread.Sleep(500);
+                EmailService.Send(emailAddress);
+            });
+        }
+
+    }
+
+    public interface IEmailService
+    {
+        void Send(string emailAddress);
+    }
+}
